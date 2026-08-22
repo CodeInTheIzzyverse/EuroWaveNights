063fb64e35547f34eb83713413122a9e51e1fc4c
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-08-22 00:21:04 -0500

    Added SoundCloud

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -4,5 +4,6 @@ export const SOCIAL = {
 	YOUTUBE: 'https://www.youtube.com/@EuroWaveNights',
 	BANDCAMP: 'https://latepassenger.bandcamp.com/',
 	SPOTIFY: '',
+	SOUNDCLOUD: 'https://soundcloud.com/latepassengermusic',
 	EMAIL: 'latepassenger.music@outlook.com'
 } as const;
